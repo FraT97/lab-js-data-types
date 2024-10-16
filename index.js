c25f0c86c5d85a3fb6709f5422d1be04e424e228
Francesco Torchia <francescotorchia10@gmail.com>
2024-10-16 17:59:27 +0200

    Exercises lesson 1

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let newStr= `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
+
+console.log(newStr)
 
 
 
@@ -23,10 +26,15 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const lastLetterPart1= part1[part1.length - 3].toUpperCase();
+const lastLetterPart2 = part2[part2.length - 1].toUpperCase();
+const newPart1 = part1.slice(0, 3) + lastLetterPart1;
+const newPart2 = part2.slice(0, 5) + lastLetterPart2;
+const result = newPart1 + newPart2;
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result)
 
 
 /*******************************************
@@ -36,10 +44,12 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal * 0.15;
+
 
 // Print out the tipAmount
 
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -48,9 +58,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomNumber= Math. random() * (10 - 1) + 1
 
 // Print the generated random number
 
+console.log(randomNumber)
 
 
 /*******************************************
@@ -61,16 +73,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
 
-const expression2 = a || b;
+const expression2 = a || b; //true
 
-const expression3 = !a && b;
+const expression3 = !a && b; //false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; //true
